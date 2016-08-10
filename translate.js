692db66582479217d510ce944570d182c0dc769b
Alex Kuzovkov <alexeykuzovkov@yandex.ru>
2016-08-10 15:38:19 +0300

    get

--- translate.js
+++ translate.js
@@ -1,41 +1,55 @@
-var argv = require('minimist')(process.argv.slice(2));
-var _ = require('underscore');
+const _ = require('underscore');
 const request = require('request');
 const colors = require('colors');
 
 const apiKey = "trnsl.1.1.20160810T103342Z.edcb645beaf697e0.f7e4610b2eca756d11e2f745ed496deb1b718ac6";
 
-if (_.has(argv, 'lang')) {
-	var translateFormat = argv['lang'];
-}
-else {
-	var translateFormat = 'en-ru';
-	console.log("Чтобы указать формат перевода:");
-	console.log("node translate.js --lang ru-en");
-	console.log("node translate.js --lang ru");
-}
+const http = require("http");
+const url = require('url');
 
-console.log("Формат перевода: "+translateFormat);
+function onRequest(req, response) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
 
-const readline = require('readline');
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout 
-});
+	//Автоматическое заполнение поля "строка для перевода"
+	var translateInputFillValue = "";
+	//Автоматическое заполнение поля "язык"
+	var langInputFillValue = "";
 
+	response.write("<html><head><meta charset='utf-8'/></head><body>");
+	if (_.has(query, 'translate')) {
+		translateInputFillValue = query['translate'];
 
-var waitForRequest = function() {
-	rl.question('Введите строку для перевода:', function(stringToTranslate) {
-		request(
-			'https://translate.yandex.net/api/v1.5/tr.json/translate?key='+apiKey+"&text="+encodeURIComponent(stringToTranslate)+"&lang="+translateFormat,	
-			function (error, response, data) {
+		var lang = 'ru-en';
+		if(_.has(query, 'lang')) {
+			if (query['lang']!=="") lang = query['lang'];
+		}
+		langInputFillValue = lang;
+
+		const uri = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='
+					+apiKey
+					+"&text="
+					+encodeURIComponent(query['translate'])
+					+"&lang="
+					+lang;
+		
+		request(uri, function (error, tresp, data) {
 			if (!error && response.statusCode == 200) {
 				let json = JSON.parse(data);
-				console.log(json["text"][0]);
-				waitForRequest();
+				_.each(json["text"], function(translatedText) {
+					response.write("<p>"+translatedText+"</p>");	
+				})
+				
+				response.write("</body><html>");
+				response.end();
 			}
 		});
-		
-	});
+	}
+	response.write("<form method='get'>"
+					+"<input type='text' name='translate' value='"+translateInputFillValue+"' placeholder='Строка для перевода'/>"
+					+"<input type='text' name='lang' value='"+langInputFillValue+"' placeholder='ru-en'/>"
+					+"<input type='submit'/></form>");
 }
-waitForRequest();
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
